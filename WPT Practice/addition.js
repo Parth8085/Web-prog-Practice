21ebc98319b025071ae5e3c20131ed1f017b5fc8
unknown <you@example.com>
2025-09-12 16:27:10 -0700

    New file added

---
+++ WPT Practice/addition.js
@@ -0,0 +1,15 @@
+function add(...a)
+{
+    let sum=0;
+    for (let i=0;i<a.length;i++)   //sum=0
+   { sum+=a[i];
+
+   }
+    
+    console.log(a.length +sum);
+
+
+}
+
+add(10,25,45,14,100)
+add(10,50)
