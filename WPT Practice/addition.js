c9a34db1100d93f91b5853781e8842c3dad62d03
unknown <you@example.com>
2025-09-12 19:28:14 -0700

    Files modified

--- WPT Practice/addition.js
+++ WPT Practice/addition.js
@@ -1,15 +1,20 @@
-function add(...a)
-{
-    let sum=0;
-    for (let i=0;i<a.length;i++)   //sum=0
-   { sum+=a[i];
+function add(...a) {
+    let sum = 0;
+    for (let i = 0; i < a.length; i++)   //sum=0
+    {
+        sum += a[i];
 
-   }
-    
-    console.log(a.length +sum);
+        if (a[i] % 2 != 0) {
+            console.log("Odd number will be: " + a[i])
+            // console.log(a[i])
+        }
+
+    }
+
+    console.log("Sum will be: " + sum);
+    console.log("Largest number will be: " +Math.max(...a))
 
 
 }
 
-add(10,25,45,14,100)
-add(10,50)
+add(10, 25, 45, 14, 100)
